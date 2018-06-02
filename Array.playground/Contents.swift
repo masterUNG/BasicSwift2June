54ada40b0037d80643b9bf0b7931ddc323af2d98
MasterUNG <phrombutr@gmail.com>
2018-06-02 11:41:21 +0700

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Array
+
+var numbersInt = [11,22,33]
+//การหา จำนวนสมาชิคของ Array
+print("จำนวนสมาชิค ==> \(numbersInt.count)")
+
+//การเพิ่ม สมาชิคบน Array
+numbersInt.append(55)
+
+//แสดงสมาชิคที่ 3
+print("สมาชิคที่ 3 ==> \(numbersInt[3])")
+
+//การลบสมาชิค
+numbersInt.remove(at: 1)
+numbersInt
+numbersInt[2]
+
+numbersInt.append(8)
+numbersInt.append(15)
+
+//การเรียงข้อมูล จากน้อยไปมาก
+numbersInt.sort()
+numbersInt
+numbersInt[2]
+
+
+
+
+
+
+
+
+
+
+
