1af4cb3bfb5067f41512bd8882a0dcf5f74693da
MasterUNG <phrombutr@gmail.com>
2018-06-02 11:57:04 +0700

    Dictionary

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Dictionary
+//การกำหนดค่า
+var nameDicString = ["key1":"Doramon", "key2":"Nopita", "key3":"MasterUNG"]
+
+var numberDicInt = ["num1": 11, "num2": 22]
+
+//การเรียกใช้
+print("ค่าของ nameDicSting ที่มีค่า key = key2 คือ \(nameDicString["key2"])")
+print("number  ==> \(numberDicInt["num1"])")
+
+
+
+
+
+
