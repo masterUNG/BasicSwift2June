c2b9b2b1ffebe30b7d43bd1370b7e04b3b2fdda9
MasterUNG <phrombutr@gmail.com>
2018-06-02 10:06:18 +0700

    Variable and Constant

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Variable and Constant
+//นี่คือการประกาศตัวแปรและค่าคงที่
+
+//Implicit Variable
+//การประการตัวแปร แบบ ใช้ ค่าเป็นตัวกำหนด Data Type
+
+var nameString = "Master UNG"
+var numberInt = 123
+var numberDouble = 3.1416
+var statusBol = true
+
+//Try To Change
+nameString = "Doramon"
+numberInt = 555
+
+//Implicit Constant คือการประกาศ ค่าคงที่ ที่ไม่สามารถแก้ไขค่าได้
+let surnameSting = "Phrombutr"
+
+//surnameSting = "NewSurName"
+
+
+
+
+
+
+
+
+
+
